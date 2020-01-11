432f8492bef9145d37cdb18a5a75371282552e03
ericteeman <ericmteeman@gmail.com>
2020-01-10 21:03:32 -0800

    Updates
    minor formatting

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 For use in determining the size and size distribution of superparamagnetic nanoparticles using Fiji and psa_r12 macro.
 
 Size and size distribution are determined from a log-normal fit of the compiled histrograms of sizes exported from the psa_r12 macro.
+
+ParticleSizeAnalyzer Optimized Settings
+- Assuming proper calibration (set scale) has already been completed
+- Settings for spherical nanoaparticles with expected average sizes from 20-30 nm
+- Interactive scaling possible, but much slower
+
+Help with scaling (interactive)     NONE
+Background removal                  Rolling Ball (r = 50 px)
+Smoothing filter                    Median r = 3 (7x7)
+Thresholding mode                   Manual (interactive)
+Separation of touching particles    Watershed filter
+Minimum diameter                    10
+Maximum diameter                    50
+Minimum circularity                 0.850
+Maximum circularity                 1
--- tem-process.R
+++ tem-process.R
@@ -28,21 +28,27 @@ combined_files <- do.call("rbind", lapply(files, function(i) {
 }))
 
 # Extract diameters as a list
-dat <- combined_files$Diameter
+dat        <- combined_files$Diameter
+
+circ.mean  <- round(mean(combined_files$Circ.),3)
+circ.sd    <- round(sd(combined_files$Circ.),3)
+
+round.mean <- round(mean(combined_files$Round),3)
+round.sd   <- round(sd(combined_files$Round),3)
 
 # Generate histogram in set range
 bin <- seq(1, 60, by = .01)
 hst <- hist(dat,
-            breaks = "FD",
-            plot = FALSE,
+            breaks      = "FD",
+            plot        = FALSE,
             warn.unused = FALSE)
 
 # For base R log-normal parameters
-df.hst <- as.data.frame(cbind(hst$mids, hst$density))
+df.hst           <- as.data.frame(cbind(hst$mids, hst$density))
 colnames(df.hst) <- c("d", "density")
 
 # Set initial fit values
-d.start <- median(dat)
+d.start    <- median(dat)
 sgma.start <- 0.05
 
 # Log-normal fitting function
@@ -65,44 +71,64 @@ colnames(df.fit) <- c("d", "fit")
 
 # Important export values -------------------------------------------------
 
-d <- round(coef(fn)[1], 1)
+d     <- round(coef(fn)[1], 1)
 sigma <- round(coef(fn)[2], 2)
-n <- length(dat)
+n     <- length(dat)
 
-export <- data.frame(sep = "-", values = c(d, sigma, n))
-colnames(export) <- c("", "")
-rownames(export) <- c("d", "\u03C3", "n")
+export           <- data.frame(d, sigma, n, circ.mean, circ.sd, round.mean, round.sd)
+colnames(export) <- c("d", "\u03C3", "n", "circ", "circ sd", "round", "round sd")
+rownames(export) <- c("")
 
 
 # Plots -------------------------------------------------------------------
 
 # Personalized theme. Can be edited to suit users prefernce
-my_theme <- function (base_size = 24, base_line_size = 1) {
+my_theme <- function (base_size = 22, base_line_size = 1) {
     theme_bw(base_size = base_size,  base_family = "") %+replace%
         theme(
-            axis.text.x = element_text(size = base_size, margin = margin(t = 0.75 * base_size, b = 0.25 * base_size), color = "black"),
-            axis.text.y = element_text(size = base_size, margin = margin(r = 0.75 * base_size, l = 0.25 * base_size), color = "black"),
-            axis.title = element_text(size = base_size, color = "black"),
-            axis.line = element_line(size = base_line_size, lineend = "square", color = "black"),
-            axis.ticks = element_line(size = base_line_size, lineend = "square", color = "black"),
-            axis.ticks.length = unit(-8, "pt"),
-            panel.border = element_blank(),
-            panel.grid = element_blank(),
-            aspect.ratio = 1,
-            legend.background = element_rect(fill = "transparent", colour = NA),
-            legend.position = c(1, 1),
+            axis.text.x = element_text(
+                size    = base_size,
+                margin  = margin(t = 0.75 * base_size, b = 0.25 * base_size),
+                color   = "black"
+            ),
+            axis.text.y = element_text(
+                size    = base_size,
+                margin  = margin(r = 0.75 * base_size, l = 0.25 * base_size),
+                color   = "black"
+            ),
+            axis.title  = element_text(size = base_size, color = "black"),
+            axis.line   = element_line(
+                size    = base_line_size,
+                lineend = "square",
+                color   = "black"
+            ),
+            axis.ticks  = element_line(
+                size    = base_line_size,
+                lineend = "square",
+                color   = "black"
+            ),
+            axis.ticks.length    = unit(-8, "pt"),
+            panel.border         = element_blank(),
+            panel.grid           = element_blank(),
+            aspect.ratio         = 1,
+            legend.background    = element_rect(fill = "transparent", colour = NA),
+            legend.position      = c(1, 1),
             legend.justification = c("right", "top"),
-            legend.direction = "vertical",
-            legend.title = element_text(size = 0.75 * base_size),
-            legend.title.align = 0.5,
-            legend.text = element_text(size = 0.75 * base_size),
-            legend.text.align = 0)
+            legend.direction     = "vertical",
+            legend.title         = element_text(size = 0.75 * base_size),
+            legend.title.align   = 0.5,
+            legend.text          = element_text(size = 0.75 * base_size),
+            legend.text.align    = 0
+        )
 }
 
 # Extract values to scale plot to appropriate region
+
+#need to rewrite to be consistent in plot area
+
 xmid <- df.hst$d[which(df.hst$density == max(df.hst$density))]
-xmin <- floor(xmid - 0.2 * xmid)
-xmax <- ceiling(xmid + 0.25 * xmid)
+xmin <- floor(xmid - 0.25 * xmid)
+xmax <- ceiling(xmid + 0.35 * xmid)
 xann <- xmid + 0.07 * xmid
 ymax <- max(df.hst$density) - 0.14 * max(df.hst$density)
 ymin <- max(df.hst$density) - 0.19 * max(df.hst$density)
@@ -116,7 +142,6 @@ p <- ggplot(df.hst, aes(x = d)) +
     labs(x = xlab, y = ylab) +
     geom_bar( aes(y = density), stat = "identity", na.rm = TRUE, fill = "white", color = "black", size = 0.5) +
     geom_line(data = df.fit, aes(x = d, y = fit), stat = "identity", size = 1, color = "red3", na.rm = TRUE) +
-    annotation_custom( tableGrob(export, theme = ttheme_minimal(base_size = 18, parse = TRUE)), xmin = xann, xmax = xmax, ymin = ymin, ymax = ymax) +
     scale_x_continuous(breaks = pretty_breaks(3), limits = c(xmin, xmax)) +
     scale_y_continuous(breaks = pretty_breaks(3)) +
     guides(color = guide_legend())
@@ -124,7 +149,7 @@ p <- ggplot(df.hst, aes(x = d)) +
 
 # Export and Display Plots ------------------------------------------------
 
-main.dir <- getwd()
+main.dir   <- getwd()
 export.dir <- "export"
 dir.create(file.path(main.dir, export.dir), showWarnings = FALSE)
 setwd(file.path(main.dir, export.dir))
@@ -135,8 +160,8 @@ write.csv(df.fit, "fit.csv", row.names = FALSE)
 
 ggsave("hist.png",
        p,
-       width = 4.5,
+       width  = 4.5,
        height = 4.5,
-       dpi = "retina")
+       dpi    = "retina")
 
 p
